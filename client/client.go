6d0f65f89ce852acae80b27519f0bf5e74e797ca
MoWeiLong <18550039021@163.com>
2023-05-24 18:11:55 +0800

    init

---
+++ client/client.go
@@ -0,0 +1,54 @@
+package client
+
+import (
+	"github.com/CloudWeOps/phoenix/logger"
+	"github.com/CloudWeOps/phoenix/logger/zap"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	"github.com/cloudweops/spark/apps/book"
+)
+
+var (
+	client *ClientSet
+)
+
+// SetGlobal todo
+func SetGlobal(cli *ClientSet) {
+	client = cli
+}
+
+// C Global
+func C() *ClientSet {
+	return client
+}
+
+// Client 客户端
+type ClientSet struct {
+	conn *grpc.ClientConn
+	log  logger.Logger
+}
+
+// Book服务的SDK
+func (c *ClientSet) Book() book.ServiceClient {
+	return book.NewServiceClient(c.conn)
+}
+
+func NewClient() (*ClientSet, error) {
+	zap.DevelopmentSetup()
+	log := zap.L()
+
+	conn, err := grpc.Dial(
+		":8089",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ClientSet{
+		conn: conn,
+		log:  log,
+	}, nil
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/cloudweops/spark/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
